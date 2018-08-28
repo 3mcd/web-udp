e35873d6d69a6e171aa70b7b86c4f341b24199a2
3mcd <eric.g.mcdaniel@gmail.com>
2018-08-27 20:19:40 -0400

    chore: got windows build working

--- tools/build.js
+++ tools/build.js
@@ -7,9 +7,6 @@ const build = async (name, dir) => {
     require("./build-babel")(dir).then(() =>
       console.log(" Transpiled and distributed source."),
     ),
-    require("./build-types")(dir).then(() =>
-      console.log(" Distributed types."),
-    ),
     require("./build-webpack")(dir).then(() =>
       console.log(" Created Webpack bundle."),
     ),
